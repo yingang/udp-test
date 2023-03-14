750ac7bc5ad8f52e716ed8ab013e57c894ce29d1
Gang Yin <1246410+yingang@users.noreply.github.com>
2023-03-14 09:12:07 +0800

    update statistical logic in server

--- src/server.rs
+++ src/server.rs
@@ -13,7 +13,7 @@ struct Args {
 
     /// Checking batch
     #[clap(long, value_parser, default_value="100000")]
-    batch: i32,
+    batch: usize,
 }
 
 fn main() -> std::io::Result<()> {
@@ -26,10 +26,10 @@ fn main() -> std::io::Result<()> {
     let mut buf: [u8; 1500] = [0; 1500];
 
     let mut expected_id: u32 = 0;
-    let mut total_lost: usize = 0;
 
     let mut batch_size: usize = 0;
-    let mut batch_count: i32 = 0;
+    let mut batch_count: usize = 0;
+    let mut batch_lost: usize = 0;
 
     let mut first_recv = true;
 
@@ -41,8 +41,8 @@ fn main() -> std::io::Result<()> {
         let received_id = ((buf[3] as u32) << 24) + ((buf[2] as u32) << 16) + ((buf[1] as u32) << 8) + buf[0] as u32;
 
         if !first_recv && received_id != expected_id {
-            println!("unexpected id: {}, expected id: {}, lost: {}", received_id, expected_id, received_id.wrapping_sub(expected_id));
-            total_lost += received_id.wrapping_sub(expected_id) as usize;
+            println!("  unexpected id: {}, expected id: {}, lost: {}", received_id, expected_id, received_id.wrapping_sub(expected_id));
+            batch_lost += received_id.wrapping_sub(expected_id) as usize;
         }
 
         if first_recv {
@@ -55,8 +55,10 @@ fn main() -> std::io::Result<()> {
             let size = batch_size as f32 / 1e6;
             let elapsed = SystemTime::now().duration_since(start_time).unwrap();
             let speed = batch_size as f64 / elapsed.as_micros() as f64;
-            println!("received: {size} MB, receving bandwidth: {speed} MB/s, total_lost: {total_lost}");
+            let lost_rate = batch_lost as f64 * 100.0f64 / (batch_count + batch_lost) as f64;
+            println!("received: {size:.2} MB, bandwidth: {speed:.2} MB/s, lost: {batch_lost} ({lost_rate:.2}%)");
             batch_count = 0;
+            batch_lost = 0;
             batch_size = 0;
             start_time = SystemTime::now();
         }
